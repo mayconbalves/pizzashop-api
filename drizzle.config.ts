f6462cf6f61b261e32d9ea99aa1fcf13606f6b18
mayconbalves <mayconbalves1@gmail.com>
2026-02-05 14:27:22 -0300

    feat(drizzle-config): created url to generate migrate

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,4 +4,12 @@ export default {
   schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dialect: "postgresql",
+  dbCredentials: {
+    host: "localhost",
+    port: 5432,
+    user: "docker",
+    password: "docker",
+    database: "pizzashop",
+    ssl: false,
+  },
 } satisfies Config;
