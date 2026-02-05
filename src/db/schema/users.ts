035a1a3ab199a66b7fdb8ed79e11dfdfceb194fc
mayconbalves <mayconbalves1@gmail.com>
2026-02-05 11:25:19 -0300

    fix: lint spaces

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -3,8 +3,10 @@ import { createId } from "@paralleldrive/cuid2";
 
 export const userRoleEnum = pgEnum("user_role", ["customer", "manager"]);
 
-export const user = pgTable("users", {
-  id: text("id").$defaultFn(() => createId()).primaryKey(),
+export const users = pgTable("users", {
+  id: text("id")
+    .$defaultFn(() => createId())
+    .primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
   phone: text("phone"),
@@ -12,4 +14,3 @@ export const user = pgTable("users", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
-
