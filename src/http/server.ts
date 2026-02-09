703f109bba6ca207e4057fac0afdf19209c76526
mayconbalves <mayconbalves1@gmail.com>
2026-02-09 08:50:39 -0300

    feat(server): imported new routers

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,34 +1,10 @@
-import { Elysia, t } from "elysia";
-import { users, restaurants } from "../db/schema";
-import { db } from "../db/connection";
+import { Elysia } from "elysia";
+import { registerRestaurant } from "./routes/register-restaurante";
+import { sendAuthLink } from "./routes/send-auth-link";
 
 const app = new Elysia()
-  .post("/restaurants", async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body;
-
-   const [manager] = await db.insert(users).values({
-      name: managerName,
-      email,
-      phone,
-      role: "manager",
-    }).returning({
-      id: users.id,
-    });
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager?.id,
-    });
-
-    set.status = 204;
-  }, {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      email: t.String({ format: "email" }),
-      phone: t.String(),
-    })
-  })
+  .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log(`🚀 Server running at http://localhost:3333`);
