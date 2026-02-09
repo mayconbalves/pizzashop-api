2397df54831894b37365ec43f4b148bba8b6bfa5
mayconbalves <mayconbalves1@gmail.com>
2026-02-09 07:52:11 -0300

    feat(server): created first router to register restaurants

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,34 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { users, restaurants } from "../db/schema";
+import { db } from "../db/connection";
 
 const app = new Elysia()
-    .get("/", () => {
-        return "Hello World"
+  .post("/restaurants", async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body;
+
+   const [manager] = await db.insert(users).values({
+      name: managerName,
+      email,
+      phone,
+      role: "manager",
+    }).returning({
+      id: users.id,
+    });
+
+    await db.insert(restaurants).values({
+      name: restaurantName,
+      managerId: manager?.id,
+    });
+
+    set.status = 204;
+  }, {
+    body: t.Object({
+      restaurantName: t.String(),
+      managerName: t.String(),
+      email: t.String({ format: "email" }),
+      phone: t.String(),
     })
+  })
 
 app.listen(3333, () => {
   console.log(`🚀 Server running at http://localhost:3333`);
