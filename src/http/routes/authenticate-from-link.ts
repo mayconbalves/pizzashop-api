f688f28cd93717779cee22c020ce7fbad21ddc57
mayconbalves <mayconbalves1@gmail.com>
2026-02-09 18:42:25 -0300

    feat(*): created authentication from link router

---
+++ src/http/routes/authenticate-from-link.ts
@@ -0,0 +1,65 @@
+import { Elysia, t } from "elysia";
+import { db } from "../../db/connection";
+import dayjs from "dayjs";
+import { auth } from "../auth";
+import { authLinks } from "../../db/schema/auth-links";
+import { eq } from "drizzle-orm";
+
+export const authenticateFromLink = new Elysia()
+  .use(auth)
+  .get(
+    "/auth-links/authenticate",
+
+    async ({ query, set, jwt: { sign} }) => {
+    const { code, redirect } = query;
+
+    const authLinkFromCode = await db.query.authLinks.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.code, code);
+      },
+    });
+
+    if (!authLinkFromCode) {
+      throw new Error("Auth link not found");
+    }
+
+    const daysSinceAuthLinkWasCreated = dayjs().diff(
+      authLinkFromCode.createdAt,
+      "days",
+    );
+
+    if (daysSinceAuthLinkWasCreated > 7) {
+      throw new Error("Auth link expired, please generate a new code.");
+    }
+
+    const managedRestaurant = await db.query.restaurants.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.managerId, authLinkFromCode!.userId);
+      },
+    });
+
+    const jwt = await sign({
+      sub: authLinkFromCode.userId,
+      restaurantId: managedRestaurant?.id,
+    });
+
+    set.cookie = {
+      auth: {
+        value: jwt,
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+        path: "/",
+      },
+    };
+
+    await db.delete(authLinks).where(eq(authLinks.code, code));
+
+    set.redirect = redirect;
+  },
+  {
+    query: t.Object({
+      code: t.String(),
+      redirect: t.String(),
+    }),
+  }
+);
