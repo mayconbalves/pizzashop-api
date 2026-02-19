1df9da332a3953162b493b3c8a6a9b473253cf20
mayconbalves <mayconbalves1@gmail.com>
2026-02-19 08:38:19 -0300

    feat(auth): created get current user method

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,10 +4,13 @@ import { sendAuthLink } from "./routes/send-auth-link";
 import { authenticateFromLink } from "./routes/authenticate-from-link";
 import { signOut } from "./routes/sign-out";
 
+import { getProfile } from "./routes/get-profile";
+
 const app = new Elysia()
   .use(registerRestaurant)
   .use(sendAuthLink)
   .use(authenticateFromLink)
+  .use(getProfile)
   .use(signOut);
 
 app.listen(3333, () => {
