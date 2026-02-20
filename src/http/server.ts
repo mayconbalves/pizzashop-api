a13bc86c54007263dcf04a1088c368f92de254c1
mayconbalves <mayconbalves1@gmail.com>
2026-02-20 16:16:42 -0300

    feat(http): created unauthorized error message

--- src/http/server.ts
+++ src/http/server.ts
@@ -13,7 +13,21 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(getProfile)
   .use(getManagedRestaurant)
-  .use(signOut);
+  .use(signOut)
+  .onError(({ code, error, set }) => {
+    switch (code) {
+      case "VALIDATION": {
+        set.status = error.status;
+
+        return error.toResponse();
+      }
+
+      default: {
+        console.error(error);
+        return new Response(null, { status: 500 });
+      }
+    }
+  });
 
 app.listen(3333, () => {
   console.log(`🚀 Server running at http://localhost:3333`);
