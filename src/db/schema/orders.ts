aa5b29735d08a7a24088ccce0e402925eee70a59
mayconbalves <mayconbalves1@gmail.com>
2026-02-22 14:59:59 -0300

    feat(schema): created order

---
+++ src/db/schema/orders.ts
@@ -0,0 +1,47 @@
+import { text, timestamp, pgTable, integer, pgEnum } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { createId } from "@paralleldrive/cuid2";
+import { users } from "./users";
+import { restaurants } from "./restaurants";
+import { orderItems } from "./order-items";
+
+export const orderStatusEnum = pgEnum("order_status", [
+  "pending",
+  "processing",
+  "delivering",
+  "delivered",
+  "canceled",
+]);
+
+export const orders = pgTable("orders", {
+  id: text("id")
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  customerId: text("customer_id").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  restaurantId: text("restaurant_id")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+  status: orderStatusEnum("status").default("pending").notNull(),
+  totalInCents: integer("total_in_cents").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+});
+
+export const ordersRelations = relations(orders, ({ one, many }) => {
+  return {
+    customer: one(users, {
+      fields: [orders.customerId],
+      references: [users.id],
+      relationName: "order_costumer",
+    }),
+    restaurant: one(restaurants, {
+      fields: [orders.restaurantId],
+      references: [restaurants.id],
+      relationName: "orders_restaurant",
+    }),
+    orderItems: many(orderItems),
+  };
+});
